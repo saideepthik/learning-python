345c9812f7637ac18dd555afc57aa024787c1d2f
saideepthik <34998473+saideepthik@users.noreply.github.com>
2018-01-09 16:51:59 +0530

    Create scale.py

---
+++ python/opencv/scale.py
@@ -0,0 +1,15 @@
+#learn resizing and saving an image
+import cv2
+import time
+
+for i in range (2,85):
+
+	print i
+	img= cv2.imread('left'+str(i)+'.jpg')
+	cv2.imshow('img',img)
+	dim = (300, 300)
+	frame = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+	cv2.imwrite('left'+str(i)+'.jpg',frame)
+	
+	
+cv2.destroyAllWindows()
